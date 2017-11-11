3902974f26f059a2837069b70e575478387a28d9
snovuda <andrey.hunt@gmail.com>
2017-11-11 08:27:37 +0200

    HTTPCACHE_IGNORE_HTTP_CODES prettify

--- top_doctors/topdoctors/settings.py
+++ top_doctors/topdoctors/settings.py
@@ -89,12 +89,5 @@ HTTPCACHE_ENABLED = True
 # 60 * 60 * 24 - one day
 HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24
 HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES = [
-    203, 204, 205, 300, 301,
-    302, 303, 304, 305, 306,
-    307, 400, 401, 402, 403,
-    404, 405, 406, 407, 408,
-    409, 410, 411, 412, 413,
-    414, 415, 416, 417, 500,
-    501, 502, 503, 504, 505]
+HTTPCACHE_IGNORE_HTTP_CODES = [x for x in range(0, 300)] + [x for x in range(400, 600)]
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
